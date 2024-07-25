777fd5b967edec7e632793d6f55753c1db83afb5
Arnaud Ruymaekers <arnaudr85@gmail.com>
2024-07-25 18:13:44 +0200

    Modified the environment shape check in run_test to be just a warning

--- olfactory_navigation/simulation.py
+++ olfactory_navigation/simulation.py
@@ -788,7 +788,8 @@ def run_test(agent: Agent,
 
     # Handle the case an specific environment is given
     if environment is not None:
-        assert environment.shape == agent.environment.shape, "The provided environment's shape doesn't match the environment has been trained on..."
+        if environment.shape != agent.environment.shape:
+            print("[Warning] The provided environment's shape doesn't match the environment has been trained on...")
         print('Using the provided environment, not the agent environment.')
     else:
         environment = agent.environment
