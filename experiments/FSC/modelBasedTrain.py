5a1f8ff96c0e76708213b0ffc885ca6e1da950b1
Liathan <liathancorvinus@gmail.com>
2024-10-17 15:21:37 +0200

    Pulito un po il codice

--- experiments/FSC/modelBasedTrain.py
+++ experiments/FSC/modelBasedTrain.py
@@ -2,8 +2,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 import cupy as cp
 from scipy.special import softmax as softmax
+from cupyx.scipy.special import softmax as softmaxGPU
 import scipy.sparse as sparse
 import cupyx.scipy.sparse as cSparse
+import cupyx.scipy.sparse.linalg as cSparseLA
 from itertools import chain
 from itertools import repeat as itReapeat
 import time
@@ -27,22 +29,16 @@ def getReachable(s, M):
     return ret
 
 
-def invT(pi, dataC, rSource, cSource, find_range, R, rho):
-    T = get_Transition_Matrix_vect(pi, dataC,rSource, cSource, find_range)
-    Tinv = cp.linalg.inv( cp.eye(SC) - gamma * cp.asarray(T)).get()
-    Vold = np.matmul(Tinv, R)
-    eta = np.matmul(rho, Tinv)
-    return Vold, eta
-
 def sparse_T_GPU(pi, dataC, rSource, cSource, find_range, R, rho, M):
     T = get_Transition_Matrix_sparse_GPU(pi, dataC, rSource, cSource, find_range, M)
+    # Da qui alla fine della funzione ci mette di più che tutta un iterazione sulla CPU
     AV = cSparse.eye(SC *M, format="csr") - gamma * T
     Aeta = cSparse.eye(SC *M, format="csr") - gamma * T.T
-    V = cSparse.linalg.spsolve(AV, R)
-    eta = cSparse.linalg.spsolve(Aeta, rho)
+    V = cSparseLA.spsolve(AV, R)
+    eta = cSparseLA.spsolve(Aeta, rho)
     return V, eta
 
-# Se ho fatto bene i test, questo dovrebbe essere il più veloce di tutti. Continuo a non capire perchè usare la GPU non mi aiuta
+# Se ho fatto bene i test, questo dovrebbe essere il più veloce di tutti.
 def sparse_T_CPU(pi, dataC, rSource, cSource, find_range, R, rho, M):
     T = get_Transition_Matrix_sparse_CPU(pi, dataC, rSource, cSource, find_range, M)
     AV = sparse.eye(SC * M, format="csr") - gamma * T
@@ -69,18 +65,20 @@ def get_Transition_Matrix_sparse_GPU(pi, pObs, rSource, cSource, find_range, M):
 
 def get_Transition_Matrix_sparse_CPU(pi, pObs, rSource, cSource, find_range, M):
     rowIdx = np.array(range(SC*M)).repeat(4*M) # Ogni riga ha 4 azioni possibili per ogni memoria
-    colIdx = np.array(list(chain.from_iterable(map(getReachable, range(SC*M), itReapeat(M))))) # Per ogni stato prendo gli stati raggiungibili e li metto in una lista
+    
+    # Per ogni stato prendo gli stati raggiungibili e li metto in una lista
+    colIdx = np.array(list(chain.from_iterable(map(getReachable, range(SC*M), itReapeat(M))))) 
+    
     toSum = np.sum(pi[None, :, :, :].T * pObs[:, :], axis = 2).T.reshape(-1)
-    # print(rowIdx.shape, colIdx.shape, toSum.shape, pObs.shape, pi.shape, pObs.shape)
+    
     tempFinal = [s for s in range(SC) if (s // 92 - rSource) ** 2 + (s % 92 -cSource) **2 < find_range**2 ]
     final = tempFinal.copy()
     for i in range(1, M):
         final += [f + i * SC for f in tempFinal]
-    # print(final)
-    # Sono gli stati finali. Ad ognuno di essi cambio gli stati raggiungibili
+    # Ad ognuno degli stati finali cambio gli stati raggiungibili.
     for i in range(len(final)):
         toSum[int(final[i]*4*M):int(final[i]+1) * 4*M] = 0.25 / M # Le coordinate doppie vengono sommate tra loro
-        colIdx[final[i]*4*M:(final[i]+1) * 4*M] = final[i]
+        colIdx[final[i]*4*M:(final[i]+1) * 4*M] = final[i] # Da uno stato finale non posso andarmene
     return sparse.csr_matrix((toSum, (rowIdx, colIdx)))
 
 
@@ -122,6 +120,8 @@ def find_grad(Q, eta, pObs):
         grad[:, 0, a] = np.sum(pObs * eta * Q[:, a], axis=1)
     return grad
 
+#Risolvere il sistema lineare con CUPY ci mette di più che un'intera iterazione di Gradient Ascent con scipy
+
 if __name__ == "__main__":
     cSource = 45.5
     rSource = 91
@@ -132,88 +132,83 @@ if __name__ == "__main__":
     maxIt = 35000
     lr = 0.01
     tol = 1e-8
-    print("Inizio: ", time.ctime(), flush=True)
     dataFile = sys.argv[1]
     folder = sys.argv[2]
     M = int(sys.argv[3])
-    os.makedirs(f"results/modelBased/M{M}/celani/{dataFile}/{folder}", exist_ok=True)
-    with cp.cuda.Device(3):
-        dataC = np.load(f"celaniData/{dataFile}.npy")
-        theta = (np.random.rand(2, M, 4*M) -0.5) * 0.5
-        theta[1, :, 0::4] += 0.5
-        theta[1, :, 2::4] += 0.5 # Bias on upwind and downwind directions
-        # theta = np.load("results/modelBased/M1/celani/fine5/alpha1e-3_noRescaled_noSubtract/theta_START.npy")
-        np.save(f"results/modelBased/M{M}/celani/{dataFile}/{folder}/theta_START", theta)
-        pi = softmax(theta, axis = 2)
-        print("THETA_START", theta)
-        print("PISTART", pi)
-        print(flush=True)
 
-        R = np.ones(SC*M) * -(1 - gamma)
-        for s in range(SC):
-            r, c = s // 92, s % 92 
-            if (r - rSource) ** 2 + (c -cSource) **2 < find_range**2:
-                R[s::SC] = 0
-        # RGPU = np.asarray(R)
-        rho = np.zeros(SC*M)
-        rho[:cols] = (1-dataC[0,:cols])/np.sum((1-dataC[0,:cols])) # Copiato dal loro
-#         T = get_Transition_Matrix_vect(pi, dataC,rSource, cSource, find_range)
-#         Tinv = cp.linalg.inv( cp.eye(SC) - gamma * cp.asarray(T)).get()
-#         Vold = np.matmul(Tinv, R)
-# #        oldObjective = np.matmul(Vold, rho)
-#         eta = np.matmul(rho, Tinv)
-        Vold, eta = sparse_T_CPU(pi, dataC, rSource, cSource, find_range, R, rho, M)
-        Vconv = Vold.copy()
-        Q = calc_Q(Vold, R)
-#        oldValueLoro = get_value(Q.reshape(-1), pi, dataC, 131*92, rho)
-        # np.save("V", Vold)
-        # np.save("eta", eta)
-        # np.save("Q", Q)
-        converged = False
-        for i in range(maxIt):
-            s = time.perf_counter()
-            grad = find_grad(Q, eta, dataC)
+    # TODO: permettere di cambiarli alla chiamata da linea di comando
+    rescale = True
+    subtract = True
+    saveDir = f"results/modelBased/M{M}/celani/{dataFile}/alpha{lr}"
+    if rescale:
+        saveDir += "_Rescale"
+    else:
+        saveDir += "_noRescale"
+    if subtract:
+        saveDir += "_Subtract"
+    else:
+        saveDir += "_noSubtract"
+    saveDir = os.path.join(saveDir, folder)
+
+    os.makedirs(saveDir, exist_ok=True)
+    output = open(os.path.join(saveDir, "output.out"), "w")
+    print("Inizio: ", time.ctime(), flush=True, file=output)
+
+    dataC = np.load(f"celaniData/{dataFile}.npy")
+    theta = (np.random.rand(2, M, 4*M) -0.5) * 0.5
+    theta[1, :, 0::4] += 0.5
+    theta[1, :, 2::4] += 0.5 # Bias on upwind and downwind directions
+    # theta = np.load("results/modelBased/M1/celani/fine5/alpha1e-3_noRescaled_noSubtract/theta_START.npy")
+    np.save(os.path.join(saveDir,"theta_START"), theta)
+    pi = softmax(theta, axis = 2)
+    print("THETA_START", theta, file=output)
+    print("PISTART", pi, file=output)
+    print(flush=True, file=output)
+
+    R = np.ones(SC*M) * -(1 - gamma)
+    for s in range(SC):
+        r, c = s // 92, s % 92 
+        if (r - rSource) ** 2 + (c -cSource) **2 < find_range**2:
+            R[s::SC] = 0
+    rho = np.zeros(SC*M)
+    rho[:cols] = (1-dataC[0,:cols])/np.sum((1-dataC[0,:cols])) # Copiato dal loro
+    Vold, eta = sparse_T_CPU(pi, dataC, rSource, cSource, find_range, R, rho, M)
+    Vconv = Vold.copy()
+    Q = calc_Q(Vold, R, M)
+    converged = False
+    for i in range(maxIt):
+        s = time.perf_counter()
+        
+        grad = find_grad(Q, eta, dataC)
+        if subtract:
             grad -= np.max(grad, axis = 2, keepdims=True)
-            # print(f"GRAD {i} ", grad)
-            # print(f"Meano GRAD {i} ", grad - np.mean(grad, axis = 2, keepdims=True))
+        if rescale:  
             theta += lr / (np.max(np.abs(grad))) * grad
-            # theta += lr * grad
-            print(f"Grad {i}: ", grad)
-            # theta -= np.mean(theta, axis = 2, keepdims=True)
-            # print("THETA dopo mean", theta)
-            pi = softmax(theta, axis = 2)
-            # print("PI", pi)
-            # T = get_Transition_Matrix_vect(pi, dataC, rSource, cSource, find_range)
-            # Tinv = cp.linalg.inv( cp.eye(SC) - gamma * cp.asarray(T)).get()
-            # V = np.matmul(Tinv, R)
-            # Vold = V
-            # eta = np.matmul(rho, Tinv)
-            V, eta = sparse_T_CPU(pi, dataC, rSource, cSource, find_range, R, rho, M)
-            Q = calc_Q(V, R)
-            e = time.perf_counter()
-            if (i+1) % 1000 == 0:
-#                objective = np.matmul(V, rho)
-#                valueLoro = get_value(Q.reshape(-1), pi, dataC, 131*92, rho)
-                delta = np.max(np.abs(V - Vconv))
-#                delta = np.abs((objective - oldObjective) / objective)
-#                delta = np.abs((valueLoro - oldValueLoro) / valueLoro)
-                print(i+1, time.ctime(), "Delta :", delta)
-                if delta < tol:
-                    print(f"Converged in {i+1} iterations")
-                    np.save(f"results/modelBased/M1/celani/{dataFile}/{folder}/theta_Conv{i+1}", theta)
-                    np.save(f"results/modelBased/M1/celani/{dataFile}/{folder}/V_Conv{i+1}", V)
-                    print("Theta END: ", theta, flush=True)
-                    print("PI END: ", pi, flush=True)
-                    converged = True
-                    break
-                Vconv = V
-#                oldObjective = objective
-#                oldValueLoro = valueLoro
-                np.save(f"results/modelBased/M1/celani/{dataFile}/{folder}/theta_{i+1}", theta)
-                np.save(f"results/modelBased/M1/celani/{dataFile}/{folder}/V_{i+1}", V)
-                print("last iteration took ", e-s, " seconds", flush=True)
-        if not converged:
-            np.save(f"results/modelBased/M1/celani/{dataFile}/{folder}/theta_{maxIt}", theta)
-            np.save(f"results/modelBased/M1/celani/{dataFile}/{folder}/V_{maxIt}", V)
-            print("Theta END not conv :", theta)
-            print("PI END not COnv", pi, flush = True)
+        else:
+            theta += lr * grad
+
+        # print(f"Grad {i}: ", grad)
+        pi = softmax(theta, axis = 2)
+        V, eta = sparse_T_CPU(pi, dataC, rSource, cSource, find_range, R, rho, M)
+        Q = calc_Q(V, R, M)
+        e = time.perf_counter()
+        if (i+1) % 1000 == 0:
+            delta = np.max(np.abs(V - Vconv))
+            print(i+1, time.ctime(), "Delta :", delta, file=output)
+            if delta < tol:
+                print(f"Converged in {i+1} iterations", file=output)
+                np.save(os.path.join(saveDir,f"theta_Conv{i+1}"), theta)
+                np.save(os.path.join(saveDir,f"V_Conv{i+1}"), V)
+                print("Theta END: ", theta, file=output)
+                print("PI END: ", pi, flush=True, file=output)
+                converged = True
+                break
+            Vconv = V
+            np.save(os.path.join(saveDir,f"theta_{i+1}"), theta)
+            np.save(os.path.join(saveDir,f"V_{i+1}"), V)
+            print("last iteration took ", e-s, " seconds", flush=True, file=output)
+    if not converged:
+        np.save(os.path.join(saveDir,f"theta_{maxIt}"), theta)
+        np.save(os.path.join(saveDir,f"V_{maxIt}"), V)
+        print("Theta END not conv :", theta, file=output)
+        print("PI END not COnv", pi, flush = True, file=output)
