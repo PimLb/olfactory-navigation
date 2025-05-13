30411ace017aa149e4defb8941c4dc7d1386b851
Liathan <liathancorvinus@gmail.com>
2025-05-13 16:43:03 +0200

    MC reward Shaping

--- experiments/FSC/modelFreeTrain.py
+++ experiments/FSC/modelFreeTrain.py
@@ -13,28 +13,35 @@ rSource = 91
 cols = 92
 rows = 131
 find_range = 1.1 # Source radius
-maxIt = 1000
+maxIt = 10000
 gamma = 0.99975
 lr = 0.01
 tol = 1e-8
 reward = -(1 -gamma)
 MC_samples = 10
-h = 0.01
+h = 1e-3
 ActionDict = np.asarray([
             [-1,  0], # North
             [ 0,  1], # East
             [ 1,  0], # South
             [ 0, -1]  # West
         ])
+
+def manhattan(pos):
+    source = np.array([91, 45.5])
+    manhattan_distance = np.sum(np.abs( pos - source))    
+    distance_to_border = max(0, manhattan_distance - 1.1)
+    return np.ceil(distance_to_border)
+
 def isEnd(s):
     return (s[0] - rSource) ** 2 + (s[1] -cSource) **2 < find_range**2
 
-def get_observation(s, pObs):
+def get_observation(s, pObs, gen):
     singleD = s[0] * cols + s[1]
-    return np.random.choice(2, p=pObs[:, singleD])
+    return gen.choice(2, p=pObs[:, singleD])
 
-def choose_action(pi, o):
-    return np.random.choice(4, p = pi[o, 0])
+def choose_action(pi, o, gen):
+    return gen.choice(4, p = pi[o, 0])
 
 def move(states, actions):
     ret = states + actions
@@ -42,27 +49,37 @@ def move(states, actions):
     ret[:, 1] = np.clip(ret[:, 1], 0, cols -1)
     return ret
 
-def getTrajectories(start, pObs, max_MC_steps, pi):
+def getTrajectories(start, pObs, max_MC_steps, pi, rng):
     num = start.shape[0]
     mapObs = [pObs for i in range(num)]
     step = start.astype(int)
     done = np.array([a for a in map(isEnd, step)])
-    obs = np.array([a for a in map(get_observation, step, mapObs)])
+    obs = np.array([a for a in map(get_observation, step, mapObs, [rng for i in range(num)])]) # FOrse cambiare generatori anche qui
     t = 0
     actions = np.zeros((num, 2), dtype=int)
     rewards = np.zeros(num)
+    d0s = np.zeros(num)
+    curD = np.zeros(num)
+    nextD = np.zeros(num)
+    for i in range(num):
+        d0s[i] = manhattan(start[i])
     while np.any(~done) and t < max_MC_steps:
         for i in range(num): # TODO: sequenziale, sarebbe da parallelizare
             if not done[i]:
-                a = choose_action(pi, obs[i])
+                a = choose_action(pi, obs[i], rng)
                 actions[i] = ActionDict[a]
             else:
                 actions[i] = np.array([0, 0])
+            curD[i] = -manhattan(step[i]) / d0s[i]
         t+= 1
         step = move(step, actions)
-        obs = np.array([a for a in map(get_observation, step, mapObs)])
+        for i in range(num):
+            nextD[i] = -manhattan(step[i]) / d0s[i]
+        obs = np.array([a for a in map(get_observation, step, mapObs, [rng for i in range(num)])]) # Forse anche qui
         done = np.array([a for a in map(isEnd, step)])
-        rewards[~done] += reward
+        # There is a missing gamma on this line. Does it matter?
+        # rewards[~done] += reward
+        rewards[~done] += gamma**t * (reward +gamma * nextD[~done] - curD[~done])
     return np.mean(rewards)
 
 if __name__ == "__main__":
@@ -72,20 +89,21 @@ if __name__ == "__main__":
     except IndexError:
         startingRow = 0
     print("Inizio: ", time.ctime(), flush=True)
-    dataC = np.load("PObs_Th5_Sx45.5_Sy91_M1_fine.npy")
-    os.makedirs(f"FSC/modelFree/M1/celani/row{startingRow}/maxIt_{maxIt}/{MC_max_steps}/{sys.argv[1]}", exist_ok=True)
+    dataC = np.load("celaniData/fine5.npy")
+    os.makedirs(f"results/modelFree/M1/row{startingRow}/maxIt_{maxIt}/{MC_max_steps}/{sys.argv[1]}", exist_ok=True)
     theta = (np.random.rand(2, 1, 4) -0.5) * 0.5
     theta[1, :, 0] += 0.5
     theta[1, :, 2] += 0.5 # Bias on upwind and downwind directions
-    theta -= np.mean(theta, axis=2, keepdims=True)
+    theta -= np.max(theta, axis=2, keepdims=True)
     # theta = np.ones((2,1,4))
     # theta[:, :, 2] = 10
     print("PI iniziale: ", softmax(theta, axis = 2))
     grad = np.ones_like(theta)
     rho = np.zeros(SC)
     rho[startingRow * cols:cols *(startingRow+1)] = (1-dataC[0,startingRow * cols:cols *(startingRow+1)])/np.sum((1-dataC[0,startingRow * cols:cols *(startingRow+1)])) # Copiato dal loro
-    np.save(f"FSC/modelFree/M1/celani/row{startingRow}/maxIt_{maxIt}/{MC_max_steps}/{sys.argv[1]}/theta_START", theta)
+    np.save(f"results/modelFree/M1/row{startingRow}/maxIt_{maxIt}/{MC_max_steps}/{sys.argv[1]}/theta_START", theta)
     i = 0
+    gens = [np.random.default_rng() for a in range(MC_samples)]
     while i < maxIt:
         s = time.perf_counter()
         pi = softmax(theta, axis = 2)
@@ -99,23 +117,24 @@ if __name__ == "__main__":
         for o in range(2):
             for a in range(4):
                 th = theta.copy()
-                th[o, 0, a] += h
+                th[o, 0, a] += h # TODO: Troppo piccolo per fare differenza sulla policy spesso
                 piCopies.append(softmax(th, axis=2))
-        args = [(starts, dataC, MC_max_steps, p) for p in piCopies]
+        args = [(starts, dataC, MC_max_steps, p, gens[id]) for id,p in enumerate(piCopies)]
         with mp.Pool(len(piCopies)) as p:
             rewardList = p.starmap(getTrajectories, args)
         for o in range(2):
             for a in range(4):
-                grad[o, 0, a] = (rewardList[0] - rewardList[a + o * 4]) / h
+                grad[o, 0, a] = (rewardList[a + o * 4 +1] - rewardList[0]) / h
 
         theta += lr * grad
-        theta -= np.mean(theta, axis=2, keepdims=True)
+        theta -= np.max(theta, axis=2, keepdims=True)
         pi = softmax(theta, axis = 2)
 
-        if np.any(grad != 0):
-            print("Grad not zero at iteration ", i, flush=True)
+        # if np.any(grad != 0):
+        #     print("Grad not zero at iteration ", i, flush=True)
         i+=1
-    np.save(f"FSC/modelFree/M1/celani/row{startingRow}/maxIt_{maxIt}/{MC_max_steps}/{sys.argv[1]}/theta_1000", theta)
+        if (i +1) % 1000 == 0:
+            np.save(f"results/modelFree/M1/row{startingRow}/maxIt_{maxIt}/{MC_max_steps}/{sys.argv[1]}/theta_{i+1}", theta)
     print(pi)
     print("Fine: ", time.ctime())
         
