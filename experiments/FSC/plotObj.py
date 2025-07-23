69b3e3fc838555486439cd6e59b95ba1b49c129a
Liathan <liathancorvinus@gmail.com>
2025-07-23 12:46:48 +0200

    dashed lines for the optimals and it uses differents colors for the objectives

--- experiments/FSC/plotObj.py
+++ experiments/FSC/plotObj.py
@@ -12,14 +12,17 @@ if __name__ == "__main__":
     plt.figure(figsize=(15, 10))
     ticks = [0, -0.1, -0.3, -0.4,-0.485, -0.6, -0.7, -0.8, -0.9, -1, -0.138, -0.197]
     totIter = 0
+    lObj = []
     for line in sys.stdin:
         obj = np.load(line[:-1])
+        lObj.append(obj)
         if obj.shape[0] > totIter:
             totIter = obj.shape[0]
+    plt.plot(range(totIter), [-0.138 for a in range(totIter)], "--", label = f"Optimal M3")
+    plt.plot(range(totIter), [-0.197 for a in range(totIter)], "--", label = f"Optimal M2")
+    plt.plot(range(totIter), [-0.485 for a in range(totIter)], "--", label = f"Optimal M1")
+    for obj in lObj:    
         plt.plot(range(obj.shape[0]), obj,marker=None if obj.shape[0] > 5 else "x",markersize = 8)
-    plt.hlines(-0.138, 0,totIter, "r", label = f"Optimal M3")
-    plt.hlines(-0.197, 0,totIter, "y", label = f"Optimal M2")
-    plt.hlines(-0.485, 0,totIter, "g", label = f"Optimal M1")
     plt.yticks(ticks)
     plt.ylim(-1, -0.1)
     plt.legend()
