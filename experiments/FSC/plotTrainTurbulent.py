357192feb6cb69f3196f498fa24835d393c783fc
Liathan <liathancorvinus@gmail.com>
2026-01-27 10:34:43 +0100

    Mark rollbacks and terminations

--- experiments/FSC/plotTrainTurbulent.py
+++ experiments/FSC/plotTrainTurbulent.py
@@ -23,6 +23,9 @@ total = 0
 finished = 0
 rollbacks = 0
 for file in sys.stdin:
+    rollbackPoint = []
+    beenKilled = False
+    hasFinished = False
     total += 1
     tmp = file.split("/")
     l = tmp[-2].split("_")
@@ -41,31 +44,47 @@ for file in sys.stdin:
     success = []
     avgSteps = []
     counted = False
+    iterations = 0
     for line in open(file[:-1]):
         if line.startswith("Episode"):
+            iterations += 1
             num = reg.findall(line)
             success.append(float(num[0]))
             avgSteps.append(float(num[1]))
         if "rollback" in line:
             rollbacks += 1 # Don't set count because up to 3 rollbacks are allowed during training (olny with TDAC)
+            rollbackPoint.append(iterations)
         if ("Error" in line or "Determinism" in line ) and not counted:
             errors += 1
             counted = True
         elif "Terminated" in line and not counted:
             killed += 1 
             counted = True
+            beenKilled = True
         elif "Total" in line and not counted:
             finished += 1
             counted = True
+            hasFinished = True
     plt.figure(figsize=(20, 10))
     plt.suptitle(name)
     
     plt.subplot(1,2,1).set_title("Success Rate")
     plt.plot(range(len(success)), success, label="success percentage")
     plt.ylim(-5, 110)
-    
+    plt.scatter(rollbackPoint, np.array(success)[rollbackPoint], marker="x", color="r")
+    if beenKilled:
+        plt.scatter(len(success)-1, success[-1], marker="*", color="k")
+    if hasFinished:
+        plt.scatter(len(success)-1, success[-1], marker="o", color="g")
+
     plt.subplot(1,2,2).set_title("Average Steps per episode")
     plt.plot(range(len(avgSteps)), avgSteps, label="Average Steps per episode")
+    plt.scatter(rollbackPoint, np.array(avgSteps)[rollbackPoint], marker="x", color="r")
+    if beenKilled:
+        plt.scatter(len(avgSteps)-1, avgSteps[-1], marker="*", color="k")
+    if hasFinished:
+        plt.scatter(len(avgSteps)-1, avgSteps[-1], marker="o", color="g")
+
     plt.ylim(-50, 10000)
     plt.savefig(f"{folder}/{name}.svg")
     plt.close()
