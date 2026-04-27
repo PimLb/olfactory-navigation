93011df691eadba9d59e5842f3d2f23ea5ba5d95
Liathan <liathancorvinus@gmail.com>
2026-04-27 15:28:46 +0200

    reinforce parallel

--- experiments/FSC/reinforceTurbulent.py
+++ experiments/FSC/reinforceTurbulent.py
@@ -6,13 +6,14 @@ import sys
 import argparse as ap
 import h5py
 import signal
+import utils
 
-ActionDict = [
+ActionDict = np.array([
             (-1,  0), # Downwind
             ( 0,  1), # Crosswind (Left when facing upwind)
             ( 1,  0), # Upwind
             ( 0, -1)  # Crosswind (Right when facing upwind)
-        ]
+        ])
 
 def natGrad(pi, obs, curMem, action):
     ret = np.zeros_like(pi)
@@ -60,11 +61,11 @@ def totalTime(end, start, file = None):
         print(f"Total time: {minutes}m:{seconds}s", file=file)
         return
     print(f"Total time: {hours}h:{minutes}m:{seconds}s", file=file)
-
+#TODO: won't work anymore
 def handleTERM(sig, frame): # Maybe not best practice, but seems to work
     e = time.perf_counter()
-    print("Terminated:", end="", file=output)
-    totalTime(e, s, output)
+    print("Terminated:", end="", file=avgOutput)
+    totalTime(e, s, avgOutput)
     exit()
 
 parser = ap.ArgumentParser()
@@ -73,10 +74,13 @@ parser.add_argument("dataPath", help="the path to a h5 file that contains the da
 parser.add_argument("lr", help="the starting learning rate", type=float)
 parser.add_argument("memories", type=int, help="The memories of the FSC")
 parser.add_argument("episodes", type=int, help="The final episode number. ")
+parser.add_argument("-c", "--clip", type=float, help="TODO")
+parser.add_argument("-p", "--parallel", type=int, help="TODO", default=1)
 parser.add_argument("--threshold", default=1e-4, help="The concentration of odor above which the agent receives a positive observation", type=float)
 parser.add_argument("-t","--thetaStart", help="the path to a .npy file containing the starting values of theta")
 parser.add_argument("--schedule", help="wheter or not to decrease the learning rate", action="store_true")
 parser.add_argument("--vanilla", help="if specified uses vanilla gradient instead on the natural", action="store_true")
+parser.add_argument("--logEvery", help="how many episodes between checkpoint and print", type=int, default=1000)
 args = parser.parse_args()
 name = args.name
 dataPath = args.dataPath
@@ -87,11 +91,14 @@ thetaPath = args.thetaStart
 schedule = args.schedule
 vanilla = args.vanilla
 threshold = args.threshold
+c = args.clip
+parallelRuns = args.parallel
+logEvery = args.logEvery
 
 odor = h5py.File(dataPath)
 rows, cols = odor['odor/0'].shape
 SC = rows * cols
-rSource, cSource = odor['source']
+src = odor['source']
 maxFrames = odor['frames'][()] # Syntax to get a scalar value from h5py
 find_range = 1.1 # Source radius
 
@@ -106,87 +113,131 @@ else:
     rho = odor['odor/0'][0] / np.sum(odor['odor/0'][0])
 
 assert M >= 1
+outputs = []
 if thetaPath is not None:
-    theta = np.load(thetaPath)
+    thetas = np.tile(np.load(thetaPath),(parallelRuns,1,1,1))
 else:
-    theta = (np.random.rand(2, M, 4*M) -0.5) * 0.5
-
-saveDir = f"storage/reinforce/turbulent/thresh_{threshold}/{"vanilla" if vanilla else "natural"}/M{M}/lr_{lr}{"_scheduled" if schedule else ""}/{name}_episodes{episodes}/"
-thetaDir = saveDir+"/thetas"
-os.makedirs(saveDir)
-os.makedirs(thetaDir)
-output = open(os.path.join(saveDir, "_results.out"), "w")
-
-grad = vanillaGrad if vanilla else natGrad
-pi = softmax(theta, axis = 2)
+    thetas = (np.random.rand(parallelRuns, 2, M, 4*M) -0.5) * 0.5
+if parallelRuns == 1:
+    saveDirs = [f"storage/reinforce/turbulent/thresh_{threshold}/{"vanilla" if vanilla else "natural"}/M{M}/lr_{lr}{"_scheduled" if schedule else ""}/{name}_episodes{episodes}/"]
+    thetaDirs = [saveDirs[0]+"/thetas"]
+else:
+    parentDir = f"storage/reinforce/turbulent/thresh_{threshold}/{"vanilla" if vanilla else "natural"}/M{M}/lr_{lr}{"_scheduled" if schedule else ""}/{name}"
+    saveDirs = [f"{parentDir}/run_{n}_episodes{episodes}/" for n in range(parallelRuns)]
+    thetaDirs = [sd+"/thetas" for sd in saveDirs]
+for sd in thetaDirs:
+    os.makedirs(sd)
+for sd in saveDirs:
+    outputs.append(open(os.path.join(sd, "_results.out"), "w"))
+if parallelRuns > 1:
+    # TODO: make a file that tracks the averages of all runs? FOr now should work
+    avgOutput = open(f"{parentDir}/average.out", "w")
+calcGrad = utils.vanillaGradVect if vanilla else utils.natGradVect
+pis = softmax(thetas, axis = 3)
 
 # Given that the reward is constant, the cumulative reward depends only on time to reach the source and we can precompute them
-cumulativeRewards = np.cumsum([gamma**a for a in range(maxSteps)] )[::-1]* reward
+cumulativeRewards = np.cumsum([gamma**a for a in range(maxSteps)]+ [-1])[::-1]* reward
 
 # Given a reward list rl, where rewards are arbitrary
 # G = 0
 # for r in reversed(rl):
 #   G = r + gamma * G
 # produces the correct G. If at each step they're prependend to a list, it computes the correct in O(n)
-
-print(f" Starting {episodes} episodes at {time.ctime()}",file=output, flush=True)
-np.save(thetaDir+"/thetaStart.npy", theta)
-
+if parallelRuns > 1:
+    print(f" Starting {parallelRuns} parallel run of {episodes} episodes at {time.ctime()}",file=avgOutput, flush=True)
+for i in range(parallelRuns):
+    print(f" Starting {parallelRuns} parallel run of {episodes} episodes at {time.ctime()}",file=outputs[i], flush=True)
+    np.save(thetaDirs[i]+"/thetaStart.npy", thetas[i])
 
 i = 0
 s = time.perf_counter()
 signal.signal(signal.SIGTERM, handleTERM)
-reached = 0
-empiricalTimeNormalized = 0
-empiricalG = 0
+reached = np.zeros(parallelRuns, dtype=int)
+stepsDone = np.zeros(parallelRuns, dtype=int)
+empiricalTimeNormalized = np.zeros(parallelRuns)
+empiricalG = np.zeros(parallelRuns)
 curLr = lr
+unbounded = False #TODO: add parameter and make it works
+dead = np.zeros(parallelRuns, dtype=bool)
 while i < episodes:
     if schedule:
         curLr = lr * 1000 / (1000 + i)
-    startCol = np.random.choice(range(cols), p = rho)
-    curState = (0, startCol) # (row,col) -> starts from most downwind row
-    curMem = 0 # starts from first memory
-    step = 0
-    t = np.random.randint(maxFrames) # Randomize on the starting time of the plume. Will make it harder to overfit the plume dynamic
-    history = np.zeros((maxSteps, 3), dtype=np.uint8) # Observation 0; Memory 1; Actions 2
-    while not isEnd(curState, rSource, cSource) and step < maxSteps:
+    startCols = np.random.choice(range(cols), p = rho, size=parallelRuns)
+    curStates = np.stack((np.zeros_like(startCols), startCols)).T # (row,col) -> starts from most downwind row
+    curMems = np.zeros_like(startCols) # starts from first memory
 
-        obs = getObservation(curState, odor, t, threshold, maxFrames)
-        action = np.random.choice(4 * M, p= pi[obs, curMem])
-        history[step, 0] = obs
-        history[step, 1] = curMem
-        history[step, 2] = action
-        curState, curMem = takeAction(curState, action, rows, cols)
+    ends = utils.getEndingStates(rows, cols, src)
+    minDist = utils.getMinDist(curStates, ends)
+
+    step = 0
+    stepsDone[:] = 0
+    t = np.random.randint(maxFrames) # Randomize on the starting time of the plume. Will make it harder to overfit the plume dynamic
+
+    # history is initialized like this because if there's a bug that makes the gradient look at wrong steps, it will raise an OutOfBound Exception
+    history = np.full((maxSteps, 3, parallelRuns), np.iinfo(np.uint8).max, dtype=np.uint8) # Observation 0; Memory 1; Actions 2
+    done = utils.isEnd(curStates, src, dead) # Won't count training that arrived to determinism
+    while np.any(~done) and step < maxSteps:
+        obs = utils.getObsTurb(curStates, t, odor, rows, cols, maxFrames)
+        actions = utils.chooseActions(obs, curMems, pis=pis)
+        history[step, 0, ~done] = obs[~done]
+        history[step, 1, ~done] = curMems[~done]
+        history[step, 2, ~done] = actions[~done]
+        curStates, curMems = utils.takeActionVect(curStates, actions, rows, cols, unbounded)
+        stepsDone[~done] += 1
+        done = utils.isEnd(curStates, src, done)
         step += 1
         t+=1
-    if step < maxSteps:
-        reached += 1
-        manhattanSource = np.floor(np.abs(startCol - cSource) + rSource) # Since we always start from row 0, startRow would be 0
-        empiricalTimeNormalized += manhattanSource / step
-        empiricalG += cumulativeRewards[-step]
-    else:
-        empiricalG += -1 # Failed episode treated as if it would never reach the source. Uncomparable with Model Based G, though.
+    reached += done & ~dead
+    empiricalTimeNormalized[done & ~dead] += minDist[done & ~dead] / stepsDone[done & ~dead]
+    empiricalG[done & ~dead] += cumulativeRewards[-stepsDone[done & ~dead]]
+    empiricalG[~done & ~dead] += -1# Failed episode treated as if it would never reach the source. Uncomparable with Model Based G, though.
     for j in range(step):
-        theta += curLr * gamma ** j * cumulativeRewards[-step+j] * grad(pi, history[j,0], history[j,1], history[j,2])
-    theta -= np.max(theta, axis =2 , keepdims=True)
-    pi = softmax(theta, axis = 2)
-    if np.any(np.isclose(pi[0], 1)):
-        e = time.perf_counter()
-        np.save(os.path.join(thetaDir , f"thetaErr_{i+1}.npy"), theta)
-        print(f"Error iteration {i+1}: reached determinism at {time.ctime()} ", file=output)
-        totalTime(e, s, output)
-        sys.exit()
-    if (i+1) % 1000 == 0:
-        print(f"Episode {i+1} done at {time.ctime()}; In the last 1000 episodes: {reached/1000:.1%}",
-              f"Time to reach normalized (only succesful): {empiricalTimeNormalized / reached if reached != 0 else 0.0}",
-              f"Empirical G: {empiricalG / 1000}", file=output, flush=True)
-        reached = 0
-        empiricalTimeNormalized = 0
-        empiricalG = 0
-        np.save(os.path.join(thetaDir , f"theta{i+1}.npy"), theta)
+        mask = -stepsDone + j < 0
+        grad = calcGrad(pis[mask], history[j,0,mask], history[j,1,mask], history[j,2,mask])
+        if c is not None:
+            utils.clipMultiGrad(grad, c) # Operation with side effect. Will compute the clipping in-place. TODO: maybe eventually check if different norms are better
+        thetas[mask] += curLr * gamma ** j * cumulativeRewards[-stepsDone[mask]+j, None, None, None] * grad
+    thetas -= np.max(thetas, axis =3 , keepdims=True)
+    pis = softmax(thetas, axis = 3)
+    if (i+1) % logEvery == 0:
+        for k in range(parallelRuns):
+            if not dead[k]:
+                print(f"Episode {i+1} done at {time.ctime()}; In the last {logEvery} episodes: {reached[k]/logEvery:.1%}",
+                    f"Time to reach normalized (only succesful): {empiricalTimeNormalized[k] / reached[k] if reached[k] != 0 else 0.0}",
+                    f"Empirical G: {empiricalG[k] / logEvery}", file=outputs[k], flush=True)
+                np.save(os.path.join(thetaDirs[k] , f"theta{i+1}.npy"), thetas[k])
+        if parallelRuns > 1:  
+            mask = reached != 0
+            #TODO: aggiungere standard error
+            #TODO: usare la maschera ~dead fa in modo che i training morti tra un checkpoint e l'altro non vengano conteggiati affatto, nemmeno per la parte "ancora buona"
+            print(f"Episode {i+1} done at {time.ctime()}; In the last {logEvery} episodes on average of {parallelRuns} agents: {np.mean(reached[~dead])/logEvery:.1%} success",
+                f"Time to reach normalized (only succesful): {np.mean(empiricalTimeNormalized[mask] / reached[mask])}",
+                f"Empirical G: {np.mean(empiricalG[~dead]) / logEvery}",
+                f"{np.count_nonzero(dead)/parallelRuns:.1%} trainings dead", file=avgOutput, flush=True)
+            
+        reached[:] = 0
+        empiricalTimeNormalized[:] = 0
+        empiricalG[:] = 0
+    if np.any(np.isclose(pis[:, 0], 1)):
+        for k in range(parallelRuns):
+            if not dead[k] and np.any(np.isclose(pis[k, 0], 1)):
+                e = time.perf_counter()
+                np.save(os.path.join(thetaDirs[k] , f"thetaErr_{i+1}.npy"), thetas[k])
+                print(f"Error iteration {i+1}: reached determinism at {time.ctime()} ", file=outputs[k])
+                totalTime(e, s, outputs[k])
+                dead[k] = True
+                if np.count_nonzero(dead) >= parallelRuns:
+                    print(f"All Dead at {time.ctime()} ", file=avgOutput)
+                    totalTime(e, s, avgOutput)
+                    print("All dead___", dead)
+                    sys.exit()
+    # print(file=ouput, flush=True)
     i+=1
 e = time.perf_counter()
-
-np.save(os.path.join(thetaDir,"thetaFinal.npy"), theta)
-totalTime(e, s, output)
-totalTime(e, s)
+for k in range(parallelRuns):
+    if not dead[k]:
+        np.save(os.path.join(thetaDirs[k],"thetaFinal.npy"), thetas[k])
+        totalTime(e, s, outputs[k])
+    # totalTime(e, s)
+if parallelRuns > 1:
+    totalTime(e, s, avgOutput)
